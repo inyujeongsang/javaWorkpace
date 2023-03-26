80bf30b0cc91c38f73fdb0e9baf296e4384906d7
injeong <injeong940202@gmail.com>
2023-03-26 22:15:53 +0900

    Initial commit

---
+++ src/chap_01/_03_Variables.java
@@ -0,0 +1,17 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "변인정";
+
+        int hour = 15;
+
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다." + "배송시간: " + hour +"시");
+
+        double score = 90.6;
+        char grade = 'A';
+        System.out.println(name +"학생의 점수는"+ score +"점 입니다." +"따라서 학점은 "+grade +"입니다.");
+    }
+}
