e81915479c0c9fa94bacb3989c910664a964997e
injeong <injeong940202@gmail.com>
2023-03-29 12:57:57 +0900

    java학습 클래스 변수 마무리(office)

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -5,4 +5,10 @@ public class BlackBox {
     String resolution; //해상도
     int price; //가격
     String color; //색상
+
+    //클래스 변수
+    //클래스 변수는 클래스의 모든 인스턴스에서 공유되기 때문에
+    // 한 인스턴스에서 값을 변경하면 다른 인스턴스에서도 그 값을 공유한다.
+    //기능 개발
+    static boolean canAutoReport = true;
 }
--- src/chap_07/_03_ClassVariables.java
+++ src/chap_07/_03_ClassVariables.java
@@ -9,5 +9,16 @@ public class _03_ClassVariables {
         BlackBox b2 = new BlackBox();
         b2.modelName = "하양이";
         System.out.println(b2.modelName);
+
+        //특정 범위를 초과하는 충돌 감지 시 자동 신고 기능 개발 여부
+        System.out.println("-개발 후-");
+        //객체로 클래스 변수에 접근할수도 있지만,
+        System.out.println(b1.modelName + "자동신고기능: " + b1.canAutoReport);
+        System.out.println(b1.modelName + "자동신고기능: " + b2.canAutoReport);
+        //클래스로 클래스 변수에 접근하는게 권장된다.
+        System.out.println("모든 블랙박스 제품 자동 신고 기능: " + BlackBox.canAutoReport);
+
+        //활용법: 어떤 클래스에 공통적으로 적용해야하는 상황이 있다면 인스턴스변수가 아니라
+        //       클래스 변수로 만드는 것이 좋다.
     }
 }
